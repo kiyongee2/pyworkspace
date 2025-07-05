9e27b1e041d2f4110bdf5d9c8da4a2222d0533a0
kiyongee2 <kiyongee2@gmail.com>
2025-07-05 11:52:15 +0900

    flask

--- 웹 크롤링/3_웹크롤링2.py
+++ 웹 크롤링/3_웹크롤링2.py
@@ -7,6 +7,17 @@ url = "https://www.seoul.go.kr/main/index.jsp"
 response = requests.get(url)
 # print(response.text)
 html = BeautifulSoup(response.text, 'html.parser')
+
+# select_one("태그이름.선택자이름") - 처음 요소 찾음
+first_li = html.select_one('li.public')
+print(first_li.text)
+
+# select('태그이름.선택자이름 > 하위태그 > 하위태그')
+all_li = html.select('div.m_service ul li')
+# print(all_li)
+print(all_li[-2].text) # 평생학습포털
+
+"""
 first_li = html.find('li', attrs={'class': 'public'})
 # print(first_li)
 
@@ -20,3 +31,4 @@ print(all_li[-2].text) # 평생학습포털
 # 전체 서비스 출력
 # for li in all_li:
 #   print(li.text)
+"""
